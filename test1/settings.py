20c90ad69b697deec6b13f2fb809f090d2d278e1
marata65 <yabbarovmarat@gmail.com>
2017-01-26 11:57:53 +0500

    Changed the HTML for the site.

--- test1/settings.py
+++ test1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'blog',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# print (STATIC_ROOT);
